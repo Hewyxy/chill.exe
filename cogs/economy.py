6cfd80f3ff52f78e7bb2a1571677f53296e60afd
hewyxy <hewyxy@gmail.com>
2026-04-27 17:31:29 -0500

    Daily command was fixed

--- cogs/economy.py
+++ cogs/economy.py
@@ -21,8 +21,8 @@ def setup(bot, db):
     #daily command to give the user a random amount of money between 850 and 1350, but only once every 24 hours
     @bot.command()
     async def daily(ctx):
+        db.get_user(ctx.author.id) 
         data = db.load_data()
-        data = db.load_data()  
         user_id = str(ctx.author.id)
         today = datetime.now(UTC).date()
 
@@ -30,13 +30,15 @@ def setup(bot, db):
             embed = discord.Embed(title="Daily Reward Already Claimed", description="You have already claimed your daily reward today. Come back tomorrow!", color=0xF50000)
             await ctx.send(embed=embed)
             return
+
         reward = random.randint(850, 1350)
         db.add_money(ctx.author.id, reward)
-        data = db.load_data()  # Reload data to get the latest changes
-        embed = discord.Embed(title="Daily Reward Claimed!", description=f"You have received ${reward} as your daily reward. Come back tomorrow for more!", color=0x10F500)
-        await ctx.send(embed=embed)
+        data = db.load_data()
         data[user_id]["DailyClaim"] = str(today)
         db.save_data(data)
+
+        embed = discord.Embed(title="Daily Reward Claimed!", description=f"You have received ${reward} as your daily reward. Come back tomorrow for more!", color=0x10F500)
+        await ctx.send(embed=embed)
 
     #profile command to show the user's balance, level, and cards in an embed
     @bot.command()
